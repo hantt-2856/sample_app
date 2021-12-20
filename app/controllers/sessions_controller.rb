952b2afb8506d98402e8283e517ca69531544398
hantt-2856 <nguyen.thi.thanh.ha@sun-asterisk.com>
2021-12-20 16:37:32 +0700

    chapter_9: Advanded login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate params[:session][:password]
       log_in user
+      do_remember user
       redirect_to user
     else
       flash.now[:danger] = t ".invalid_email_password_combination"
@@ -15,5 +16,11 @@ class SessionsController < ApplicationController
   def destroy
     log_out
     redirect_to root_url
+  end
+
+  private
+
+  def do_remember user
+    params[:session][:remember_me] == "1" ? remember(user) : forget(user)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save{email.downcase!}
   validates :name, presence: {message: :presence_message},
     length: {maximum: Settings.length.max_50}
@@ -14,5 +15,26 @@ class User < ApplicationRecord
               BCrypt::Engine.cost
             end
     BCrypt::Password.create string, cost: cost
+  end
+
+  class << self
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember_me
+    self.remember_token = User.new_token
+    update_column :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest
+
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_column :remember_digest, nil
   end
 end
